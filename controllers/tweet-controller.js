77386f095bf2df4be6bf1fa7a20b7bb5272f9417
WIlson <eaz73727@gmail.com>
2022-10-05 14:55:24 +0800

    fix: GET /api/tweets

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,6 +1,23 @@
+const { Tweet, User, sequelize } = require('../models')
 const tweetController = {
-  getTweet: (req, res) => {
-    res.send('tweet')
+  getTweets: async (req, res, next) => {
+    try {
+      const tweets = await Tweet.findAll({
+        include: {
+          model: User, attributes: ['id', 'name', 'account', 'avatar']
+        },
+        attributes: ['id', 'description', 'createdAt',
+          [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'), 'replyCount'],
+          [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'), 'likeCount']
+        ],
+        order: [['createdAt', 'DESC']],
+        nest: true,
+        raw: true
+      })
+      return res.status(200).json(tweets)
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
